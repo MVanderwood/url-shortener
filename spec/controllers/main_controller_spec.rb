8bbf63c492c7707d8f6ca6cc9f38d9fabf42f830
MVanderwood <m.b.vanderwood@gmail.com>
2016-08-07 14:56:12 -0500

    Add test coverage for MainController.

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe MainController, :type => :controller do
+  describe 'GET #home' do
+    it 'should return http status success' do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
 
+    it 'should render the home page' do
+      get :home
+      expect(response).to render_template(:home)
+    end
+  end
 end
