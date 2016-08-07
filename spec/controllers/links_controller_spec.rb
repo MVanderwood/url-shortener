c0aad103fdd9d6d83aba12de7529d997185fc836
MVanderwood <m.b.vanderwood@gmail.com>
2016-08-07 16:12:46 -0500

    Fix LinksController tests and clear some cruft from the unit tests.

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe LinksController, :type => :controller do
   describe 'GET #home' do
     it 'should return http status success' do
-      get :home
+      get :new
       expect(response).to have_http_status(:success)
     end
 
-    it 'should render the home page' do
-      get :home
-      expect(response).to render_template(:home)
+    it 'should render the new page' do
+      get :new
+      expect(response).to render_template(:new)
     end
 
     it 'should assign a new instance of Link to @link' do
-      get :home
+      get :new
       expect(assigns(:link)).to be_a_kind_of(Link)
     end
   end
